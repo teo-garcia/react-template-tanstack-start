d8321360fd3f4333198d3ff856814be44f5f7cc5
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:33:02 -0500

    refactor: adopt defineConfig syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import base from '@teo-garcia/eslint-config-shared/base'
+import { defineConfig } from 'eslint/config'
 import playwright from '@teo-garcia/eslint-config-shared/playwright'
 import react from '@teo-garcia/eslint-config-shared/react'
 
-export default [
+export default defineConfig([
   { ignores: ['src/routeTree.gen.ts', '.output/**'] },
   ...base,
   ...react,
   ...playwright,
-]
+])
