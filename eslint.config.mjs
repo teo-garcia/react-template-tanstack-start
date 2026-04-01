4849072522f7fcbc85a935f6d6a2fcae5e46a6ed
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:38:19 -0500

    chore: preserve eslint config import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line simple-import-sort/imports
 import base from '@teo-garcia/eslint-config-shared/base'
 import { defineConfig } from 'eslint/config'
 import playwright from '@teo-garcia/eslint-config-shared/playwright'
