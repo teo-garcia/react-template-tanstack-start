83b814ea6dae11f52d4f952f58e59f84a8d1e729
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-02 10:12:11 -0500

    fix: use route component in page test

--- src/routes/index.test.tsx
+++ src/routes/index.test.tsx
@@ -1,15 +1,8 @@
+/* eslint-disable simple-import-sort/imports */
 import axe from 'axe-core'
-import { Zap } from 'lucide-react'
 
+import { HomePage } from './index'
 import { render } from '~/lib/test/render'
-
-function HomePage() {
-  return (
-    <section className='flex h-screen flex-col items-center justify-center gap-y-16'>
-      <Zap className='size-48 text-primary lg:size-56 xl:size-72' />
-    </section>
-  )
-}
 
 test('HomePage renders with providers', () => {
   const { container } = render(<HomePage />)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,7 +14,7 @@ export const Route = createFileRoute('/')({
   component: HomePage,
 })
 
-function HomePage() {
+export function HomePage() {
   return (
     <section className='flex h-screen flex-col items-center justify-center gap-y-16'>
       <Zap className='size-48 text-primary lg:size-56 xl:size-72' />
