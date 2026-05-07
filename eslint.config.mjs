202af13be8d5f2203750d83e5850f01976d420f6
teo-garcia <juanmateogarcia96@gmail.com>
2026-05-06 21:41:42 -0500

    chore: remove stale eslint-disable simple-import-sort comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
-// eslint-disable-next-line simple-import-sort/imports
 import base from '@teo-garcia/eslint-config-shared/base'
-import { defineConfig } from 'eslint/config'
 import playwright from '@teo-garcia/eslint-config-shared/playwright'
 import react from '@teo-garcia/eslint-config-shared/react'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   { ignores: ['src/routeTree.gen.ts', '.output/**'] },
